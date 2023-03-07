803b75379f24b85daead868f75997a5ccb0f7c22
ITkieltyk <kieltyk+gh@gmail.com>
2023-03-07 01:12:32 +0100

    Comments and ball style

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ field.style.height = screen.height * 0.8 + "px";
 const ballPosition = ([xpos, ypos] = [0, 0]);
 console.log(screen.height);
 let intervalID;
+let ballColors = ["#00aa00", "#aa0000", "#00aaaa", "#0000aa", "#aaaa00"];
 
 // Ball creation and style
 const ball = document.createElement("div");
 ball.style.borderRadius = "50%";
-ball.style.backgroundImage = "radial-gradient(#b15e0c, #990022)";
+ball.style.backgroundImage = `radial-gradient(${ballColors.toString()})`;
 ball.style.width = ballSize + "px";
 ball.style.height = ballSize + "px";
 ball.style.position = "absolute";
 ball.style.top = ypos;
 ball.style.left = xpos;
+ball.className = "ball";
 field.appendChild(ball);
 
 // ball movement start function
@@ -40,6 +42,9 @@ function ballMovementStart(event) {
       parseInt(ball.style.top) < 0
     ) {
       directionVer = directionVer * -1;
+      ballColors.push(ballColors[0]);
+      ballColors.shift();
+      ball.style.backgroundImage = `radial-gradient(${ballColors.toString()})`;
     }
 
     if (
@@ -47,32 +52,43 @@ function ballMovementStart(event) {
       parseInt(ball.style.left) > parseInt(field.style.width) - ballSize
     ) {
       directionHor = directionHor * -1;
+      ballColors.push(ballColors[0]);
+      ballColors.shift();
+      ball.style.backgroundImage = `radial-gradient(${ballColors.toString()})`;
     }
   }, 16);
 }
 
 // ball stop function
 function ballMovementStop() {
-  for (let i = 0; i < intervalID + 1; i++) {
-    clearInterval(i);
-    field.getElementsByClassName("text")[0].innerText =
-      "Mouse click to start again";
-  }
+  clearInterval(intervalID);
+  field.getElementsByClassName("text")[0].innerText =
+    "Tap or click to start again";
+  // }
 }
 field.addEventListener("click", ballMovementStart);
 field.addEventListener("dblclick", ballMovementStop);
 
 function positionClick(event) {
+  // Getting click global position
+
   const posX = event.clientX;
   const posY = event.clientY;
-  console.log("position left " + posX);
-  console.log("position top " + posY);
+
+  //  Correction of click position in correspondence to field size and position
   const posXcorr = posX - (screen.width * 0.2) / 2;
   const posYcorr = posY - 15;
+  // Setting of ball movement angle and direction
+
   if (posYcorr > ypos) {
     directionVer = 5;
   } else {
     directionVer = -5;
   }
   directionHor = (directionVer * (posXcorr - xpos)) / (posYcorr - ypos);
+
+  //  Unifying ball speed at every click position
+  const ballSpeed = (directionHor ** 2 + directionVer ** 2) ** 0.5;
+  directionHor = (directionHor * 7) / ballSpeed;
+  directionVer = (directionVer * 7) / ballSpeed;
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+Ball animation app
+
+Made as excercise for JS classes
+
+Technologies used: HTML, CSS, JS
+JS uses: setInterval, DOM manipulation, screen object properties
+
+Instructions are displayed at center of app field
